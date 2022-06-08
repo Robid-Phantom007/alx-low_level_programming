dd82e6902dfb96db29895d987ed01e39b8c996d5
Dibor Solomon <robidnomolos@gmail.com>
2022-06-08 03:39:39 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,52 @@
 #include "main.h"
+
 /**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
+ *
+ * Return: Number matrix
  */
 void print_times_table(int n)
 {
+	int a, b, c;
 
-	return (0);
+	if (n >= 0 && n <= 14)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				c = a * b;
+				if (c > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((c / 100) + '0');
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else if (c > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((c / 10) % 10) + '0');
+					_putchar((c % 10) + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(c + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
+
