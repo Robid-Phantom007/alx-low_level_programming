b4cb1c7ebb7b5e051ae4fea62904369cfe26cac2
Dibor Solomon <robidnomolos@gmail.com>
2022-06-24 11:47:20 -0700

    task 4 on 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Prints the addition of positive numbers,
- * followed by a new line.
+ *        followed by a new line.
  * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -17,10 +17,15 @@ int main(int argc, char *argv[])
 
 	for (num = 1; num < argc; num++)
 	{
-		if (argv[num][digit] < '0' || argv[num][digit] > '9')
+		for (digit = 0; argv[num][digit]; digit++)
 		{
-			printf("Error\n");
-			return (1);
+
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+
+				printf("Error\n");
+				return (1);
+			}
 		}
 
 		sum += atoi(argv[num]);
