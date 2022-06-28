9da1f2f9ef14f00ecb7eba696f91e500a8064332
Dibor Solomon <robidnomolos@gmail.com>
2022-06-28 01:26:15 -0700

    Task on 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,12 +3,12 @@
 
 /**
  * _strdup - Returns a pointer to a newly-allocated space in memory
- * 		containing a copy of the string given as parameter.
+ *		 containing a copy of the string given as parameter.
  * @str: The string to be copied.
  *
  *
  * Return: If str == NULL or insufficient memory is available - NULL.
- * 		Otherwise - a pointer to the duplicated string.
+ *		 Otherwise - a pointer to the duplicated string.
  */
 char *_strdup(char *str)
 {
